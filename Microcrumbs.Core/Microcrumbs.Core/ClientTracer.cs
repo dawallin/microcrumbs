44405eda8f47115c02ab041245d280b91f3ae8ce
dawallin <dawa2323@hotmail.com>
2015-10-13 03:59:21 +0200

    ClientTracer does not update threadContext

--- Microcrumbs.Core/Microcrumbs.Core/ClientTracer.cs
+++ Microcrumbs.Core/Microcrumbs.Core/ClientTracer.cs
@@ -2,13 +2,11 @@
 {
     public class ClientTracer : IClientTracer
     {
-        private readonly IThreadContext _threadContext;
         private readonly ISpanContextFactory _spanContextFactory;
         private readonly ISpanSubmitter _spanSubmitter;
 
-        public ClientTracer(IThreadContext threadContext, ISpanContextFactory spanContextFactory, ISpanSubmitter spanSubmitter)
+        public ClientTracer(ISpanContextFactory spanContextFactory, ISpanSubmitter spanSubmitter)
         {
-            _threadContext = threadContext;
             _spanContextFactory = spanContextFactory;
             _spanSubmitter = spanSubmitter;
         }
@@ -16,7 +14,6 @@
         public SpanContext StartClientSpan(string serviceName)
         {
             var clientSpanContext = _spanContextFactory.NewSpan();
-            _threadContext.Set(clientSpanContext);
             _spanSubmitter.Send(SpanType.ClientSend, clientSpanContext);
 
             return clientSpanContext;
