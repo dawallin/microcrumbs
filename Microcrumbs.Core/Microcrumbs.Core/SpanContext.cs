7632f00824ce28d9aa7ac144ff39dc87b5dda48e
dawallin <dawa2323@hotmail.com>
2015-10-13 02:24:26 +0200

    Nullable ids in SpanContext

--- Microcrumbs.Core/Microcrumbs.Core/SpanContext.cs
+++ Microcrumbs.Core/Microcrumbs.Core/SpanContext.cs
@@ -8,7 +8,7 @@ namespace Microcrumbs.Core
 {
     public class SpanContext
     {
-        public SpanContext(string serviceName, ulong traceId, ulong parentId, ulong spanId)
+        public SpanContext(string serviceName, ulong? traceId, ulong? parentId, ulong? spanId)
         {
             ServiceName = serviceName;
             TraceId = traceId;
@@ -17,8 +17,8 @@ namespace Microcrumbs.Core
         }
 
         public string ServiceName { get; private set; }
-        public ulong TraceId { get; private set; }
-        public ulong ParentId { get; private set; }
-        public ulong SpanId { get; private set; }
+        public ulong? TraceId { get; private set; }
+        public ulong? ParentId { get; private set; }
+        public ulong? SpanId { get; private set; }
     }
 }
